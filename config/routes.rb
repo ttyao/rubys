d642e36c8b9ab6d707d2c30a735cc126ada8f995
Henry Yao <henryy@gmail.com>
2014-09-09 08:41:57 -0700

    sign in

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,15 @@ Rails.application.routes.draw do
 
   root to: 'static_pages#home'
 
+  resources :users
+  resources :sessions, only: [:new, :create, :destroy]
   match '/help', to: 'static_pages#help', via: 'get'
 
   match '/about', to: 'static_pages#about', via: 'get'
   match '/signup', to: 'users#new', via: 'get'
+  match '/signin', to: 'sessions#new', via: 'get'
+  match '/signout', to: 'sessions#destroy', via: 'delete'
 
-  resources :users
 
   resources :microposts
 
