c641bd17e92e892237a1d839713bd231cadfeed4
Henry Yao <henryy@gmail.com>
2014-09-09 17:30:39 -0700

    Finish user edit/update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :signed_in_user, only: [:edit, :update]
+  before_action :correct_user, only: [:edit, :update]
   def new
     @user = User.new
   end
@@ -23,7 +25,19 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
+  end
+
+  def update
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    redirect_to root_path
   end
 
   private
@@ -31,5 +45,14 @@ class UsersController < ApplicationController
     def user_params
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
+    end
+
+    def signed_in_user
+      redirect_to signin_url, notice: "Please sign in." unless signed_in?
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_path) unless current_user?(@user)
     end
 end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -8,6 +8,43 @@ describe "UserPages" do
     it {should have_title(full_title('Sign up'))}
   end
 
+  describe "edit" do
+    let(:user) {FactoryGirl.create(:user)}
+    before do
+      sign_in user
+      visit edit_user_path(user)
+    end
+
+    describe "page" do
+      it {should have_content("Update your profile")}
+      it {should have_title("Edit user")}
+      it {should have_link("change", href: "http://gravatar.com/emails")}
+    end
+
+    describe "with valid information" do
+      let(:new_user) { "New Name" }
+      let(:new_email) { "new@gmail.com" }
+      before do
+        fill_in "Name", with: new_user
+        fill_in "Email", with: new_email
+        fill_in "Password", with: user.password
+        fill_in "Confirm Password", with: user.password
+        click_button "Save changes"
+      end
+
+      it {should have_title new_user}
+      it {should have_selector 'div.alert.alert-success'}
+      it {should have_link 'Sign out', href: signout_path}
+      specify {expect(user.reload.name).to eq new_user}
+      specify {expect(user.reload.email).to eq new_email}
+    end
+
+    describe "with invalid information" do
+      before {click_button "Save changes"}
+      it {should have_content "error"}
+    end
+  end
+
   describe "profile page" do
     let(:user) { FactoryGirl.create(:user)}
     before {visit user_path(user)}
